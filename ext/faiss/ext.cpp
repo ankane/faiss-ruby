bd18be16086fca5e0468eceec683d1c7a6f7325b
Andrew Kane <andrew@chartkick.com>
2020-03-08 18:02:27 -0700

    Added todos [skip ci]

--- ext/faiss/ext.cpp
+++ ext/faiss/ext.cpp
@@ -31,21 +31,25 @@ uint8_t* uint8_array(Rice::Object o)
   return (uint8_t*) s.c_str();
 }
 
+// TODO return Numo::SFloat
 Rice::String result(float* ptr, int64_t length)
 {
   return Rice::String(std::string((char*) ptr, length * sizeof(float)));
 }
 
+// TODO return Numo::UInt8
 Rice::String result(uint8_t* ptr, int64_t length)
 {
   return Rice::String(std::string((char*) ptr, length * sizeof(uint8_t)));
 }
 
+// TODO return Numo::Int32
 Rice::String result(int32_t* ptr, int64_t length)
 {
   return Rice::String(std::string((char*) ptr, length * sizeof(int32_t)));
 }
 
+// TODO return Numo::Int64
 Rice::String result(int64_t* ptr, int64_t length)
 {
   return Rice::String(std::string((char*) ptr, length * sizeof(int64_t)));
