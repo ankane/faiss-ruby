a7a2aca41b57188ab306956d28371c0af9174162
Andrew Kane <andrew@ankane.org>
2021-05-21 21:53:04 -0700

    Use find_header

--- ext/faiss/extconf.rb
+++ ext/faiss/extconf.rb
@@ -5,8 +5,7 @@ abort "LAPACK not found" unless have_library("lapack")
 abort "OpenMP not found" unless have_library("omp") || have_library("gomp")
 
 numo = $LOAD_PATH.find { |v| File.exist?("#{v}/numo/numo/narray.h") }
-$INCFLAGS << " -I#{numo}/numo" if numo
-abort "Numo not found" unless have_header("numo/narray.h")
+abort "Numo not found" unless numo && find_header("numo/narray.h", "#{numo}/numo")
 
 $CXXFLAGS << " -std=c++17 $(optflags) -DFINTEGER=int " << with_config("optflags", "-march=native")
 
@@ -15,7 +14,7 @@ vendor = File.expand_path("../../vendor/faiss", __dir__)
 
 $srcs = Dir["{#{ext},#{vendor}/faiss,#{vendor}/faiss/{impl,invlists,utils}}/*.{cpp}"]
 $objs = $srcs.map { |v| v.sub(/cpp\z/, "o") }
-$INCFLAGS << " -I#{vendor}"
+abort "Faiss not found" unless find_header("faiss/Index.h", vendor)
 $VPATH << vendor
 
 create_makefile("faiss/ext")
