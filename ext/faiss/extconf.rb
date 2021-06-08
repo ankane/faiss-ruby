a2bcea57d29a43ce55e75582af40d66b33c628fb
Andrew Kane <andrew@ankane.org>
2021-06-08 01:26:05 -0700

    Improved ext pattern

--- ext/faiss/extconf.rb
+++ ext/faiss/extconf.rb
@@ -5,8 +5,8 @@ abort "BLAS not found" unless have_library("blas")
 abort "LAPACK not found" unless have_library("lapack")
 abort "OpenMP not found" unless have_library("omp") || have_library("gomp")
 
-numo = $LOAD_PATH.find { |v| File.exist?("#{v}/numo/numo/narray.h") }
-abort "Numo not found" unless numo && find_header("numo/narray.h", "#{numo}/numo")
+numo = File.join(Gem.loaded_specs["numo-narray"].require_path, "numo")
+abort "Numo not found" unless find_header("numo/narray.h", numo)
 
 $CXXFLAGS << " -std=c++17 $(optflags) -DFINTEGER=int " << with_config("optflags", "-march=native")
 
