b1551a17149f912efff4a1c3ef38e2dbc2629779
Andrew Kane <andrew@ankane.org>
2026-03-02 21:13:28 -0800

    Fixed CI

--- ext/faiss/extconf.rb
+++ ext/faiss/extconf.rb
@@ -1,12 +1,15 @@
 require "mkmf-rice"
 require "numo/narray"
 
+cmake_args = []
+
 # libomp changed to keg-only
 # https://github.com/Homebrew/homebrew-core/issues/112107
 if RbConfig::CONFIG["host_os"] =~ /darwin/i
   brew_prefix = RbConfig::CONFIG["host_cpu"] =~ /arm|aarch64/i ? "/opt/homebrew" : "/usr/local"
   find_library("omp", nil, "#{brew_prefix}/opt/libomp/lib")
   find_header("omp.h", "#{brew_prefix}/opt/libomp/include")
+  cmake_args += ["-DOpenMP_CXX_FLAGS=-Xpreprocessor -fopenmp -I#{brew_prefix}/opt/libomp/include", "-DOpenMP_CXX_LIB_NAMES=omp", "-DOpenMP_omp_LIBRARY=#{brew_prefix}/opt/libomp/lib/libomp.dylib"]
 end
 
 abort "BLAS not found" unless have_library("blas")
@@ -32,7 +35,7 @@ $CXXFLAGS += " -fopenmp"
 ext = File.expand_path(".", __dir__)
 vendor = File.expand_path("../../vendor/faiss", __dir__)
 
-system "cmake", "-S", vendor, "-B", "build", "-DFAISS_OPT_LEVEL=dd", "-DFAISS_ENABLE_GPU=OFF", "-DFAISS_ENABLE_PYTHON=OFF", "-DFAISS_ENABLE_MKL=OFF", "-DFAISS_ENABLE_EXTRAS=OFF", "-DBUILD_TESTING=OFF", "-DCMAKE_INSTALL_PREFIX=install"
+system "cmake", "-S", vendor, "-B", "build", "-DFAISS_OPT_LEVEL=dd", "-DFAISS_ENABLE_GPU=OFF", "-DFAISS_ENABLE_PYTHON=OFF", "-DFAISS_ENABLE_MKL=OFF", "-DFAISS_ENABLE_EXTRAS=OFF", "-DBUILD_TESTING=OFF", "-DCMAKE_INSTALL_PREFIX=install", *cmake_args
 system "cmake", "--build", "build"
 system "cmake", "--install", "build"
 abort "Faiss not found" unless find_header("faiss/Index.h", "install/include")
